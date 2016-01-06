99e5ed2dd870ea803bd2d7bbe657bf29fd85ec43
Joel Gimbel <jgimbel@live.com>
2016-01-06 12:04:23 -0600

    adding websocket serverside

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
-var express = require('express')
+let express = require('express')
+  , url = require('url')
   , path = require('path')
   , app = express()
+  , server = require('http').createServer(app)
+  , WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ server: server })
   , engine = require('react-view-engine')
   , ytdl = require('ytdl-core')
+  , emptyQueue = { queue: [], playing: null, played: [] }
+  , redux = require('redux')
+  , applyMiddleware = redux.applyMiddleware
+  , createStore = redux.createStore
+  , thunk = require('redux-thunk')
+  , reducer = require('../shared/reducers')
+
+
 require('babel-register')({extensions: ['.js', '.jsx']})
 engine.setLayout('layout')
 
@@ -13,12 +25,23 @@ app.set("view engine", "jsx")
 app.set("views", __dirname+"/../shared/components")
 app.set("views cache", false)
 
+
+let locations = []
+
+
 app.use(express.static(path.join(__dirname, '..', '.build')))
 
 app.get('/', (req, res) => res.render('home'))
 
 app.get('/station/:station', (req, res) => {
-    res.render('station', {queue: [], playing: null, played: []})
+  let store
+  for(let loc of locations){
+      if(loc.url == req.path){
+          store = loc.store
+      }
+  }
+  
+    res.render('station', store || emptyQueue)
 })
 
 app.get('/song', (req, res) => {
@@ -44,6 +67,30 @@ app.get('/song', (req, res) => {
     })
 })
 
-var server = require('http').createServer(app)
+
+wss.on('connection', function connection(ws) {
+  var location = url.parse(ws.upgradeReq.url, true)
+  let store
+  for(let loc of locations){
+      if(loc.path == location.path){
+          store = loc.store
+      }
+  }
+  if(!store){
+      store = { store: applyMiddleware(thunk)(createStore)(reducer,  emptyQueue) }
+      locations.push(Object.assign({}, location, { store }))
+  }
+  
+  // you might use location.query.access_token to authenticate or share sessions
+  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
+  ws.on('message', function incoming(message) {
+    store.dispatch(message)
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
+
 server.listen(process.env.PORT || 8000, process.env.IP || '127.0.0.1')
 server.on('listening', () => console.log('listening'))
